bb1c612a7063c4baa5c5ceb65d4a7d0297a83506
SinghaSougata <91247187+SinghaSougata@users.noreply.github.com>
2022-05-31 20:20:25 -0700

    colorFlipper

---
+++ logic.js
@@ -0,0 +1,17 @@
+const colors=['red','blue','green','pink','yellow'];
+
+
+const btn=document.querySelector('#btn');
+const color=document.querySelector('.color');
+
+
+btn.addEventListener("click",function(){
+      const randomNumber=getRandomNumber();
+
+    document.body.style.backgroundColor=colors[randomNumber];
+    color.textContent=colors[randomNumber];
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*colors.length);
+}
